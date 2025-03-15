0938f2ee829da3cd694d2998b35d9d3f32969555
vishalmrode <vishalmrode@outlook.com>
2025-03-15 23:55:57 +0530

    Add example demonstrating function execution within the global context in JavaScript

--- JSPractice/ExecutionContext.js
+++ JSPractice/ExecutionContext.js
@@ -45,3 +45,19 @@ function outer() {
 }
 
 outer();
+
+var x = 10;
+
+function A() {
+  console.log("Start function A");
+
+  function B() {
+    console.log("In function B");
+  }
+
+  B();
+}
+
+A();
+
+console.log("GlobalContext");
