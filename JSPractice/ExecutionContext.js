61f968176bdc99e199f25495697f77cc466bf63c
vishalmrode <vishalmrode@outlook.com>
2025-03-15 23:55:31 +0530

    Add comments explaining the global execution context in JavaScript

--- JSPractice/ExecutionContext.js
+++ JSPractice/ExecutionContext.js
@@ -1,5 +1,9 @@
 // ExecutionContext.js
 
+//The global execution context is the default or first execution context that is created by the JavaScript engine before any code is executed(i.e, when the file first loads in the browser).
+//All the global code that is not inside a function or object will be executed inside this global execution context.
+// Since JS engine is single threaded there will be only one global environment and there will be only one global execution context.
+
 // Function to demonstrate the creation of execution contexts
 function firstFunction() {
   console.log("Entering firstFunction");
