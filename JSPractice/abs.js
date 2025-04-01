6501c6a7731c0cd5efef18b5849919512f2102a1
vishalmrode <vishalmrode@outlook.com>
2025-04-01 12:34:03 +0530

    Add PDFs

---
+++ JSPractice/abs.js
@@ -0,0 +1,11 @@
+// let fruits = ['apple', 'banana', 'orange'];
+// fruits.forEach(function(fruit, index) {
+//   console.log(index + ': ' + fruit);
+// });
+// // Output:
+// // 0: apple
+// // 1: banana
+// // 2: orange
+
+let fruit = ["apple", "banan", "kiwi"];
+console.table(fruit);
